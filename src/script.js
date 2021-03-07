8f2760772d21361007ac3ca00e6fcd87d8502d2c
ajawashington <ajawashington13@gmail.com>
2021-03-06 20:42:55 -0600

    :sparkles: rotation on object

--- src/script.js
+++ src/script.js
@@ -1,21 +1,56 @@
 import * as THREE from 'three'
 import './style.css'
 
-//* Orange Cube
 //! SCENE 
+//* Orange Cube
 const scene = new THREE.Scene()
 
-//! MESH
-
+//! OBJECTS
 //* Shape
 const geometry = new THREE.BoxGeometry(1, 1, 1)
 
-//* It Looks 
+//* How It Looks 
 const material = new THREE.MeshBasicMaterial({color: 'orange'})
 
-//* Create and Add Mesh
+//* Create Mesh
 const mesh = new THREE.Mesh(geometry, material)
+
+//* Transform Object
+// mesh.position.x = 0.7
+// mesh.position.y = - 0.6
+// mesh.position.z = 1
+//* Another way to do it
+mesh.position.set(0.7, -0.6, 1)
+
+//* Normalize position values
+// mesh.position.normalize()
+
+//* Add Mesh to Scene
 scene.add(mesh)
+
+//* Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+//* Another way to do it
+mesh.scale.set(2, 0.5, 0.5)
+
+//* Rotation
+
+//* How to fix a locked axis
+mesh.rotation.reorder('YXZ')
+
+mesh.rotation.x = Math.PI * 0.25
+mesh.rotation.y = Math.PI * 0.25
+mesh.rotation.z = Math.PI * 0
+//* Another way to do it
+// mesh.scale.set(2, 0.5, 0.5)
+
+
+//* Axes Helper 
+//* red = x green = y blue = z
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 //! SIZES 
 const sizes = {
@@ -27,12 +62,10 @@ const sizes = {
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 
 //* Position  x = side  y = up/down  z = forward/backward
+// camera.position.x = 1
+// camera.position.y = 1
 camera.position.z = 3
-// camera.position.y = 2
-// camera.position.x = 2
 
-//* Rotation
-//* Scale
 
 //* Add Camera to Scene
 scene.add(camera)
@@ -42,6 +75,7 @@ const canvas = document.querySelector('.webgl')
 const renderer = new THREE.WebGLRenderer({
     canvas: canvas
 })
+
 
 //* Resize
 renderer.setSize(sizes.width, sizes.height)
